65c2bafef0a5a967f59d2e66c18464a6fbe8928f
Joilson Izael <90202358+JoilsonI@users.noreply.github.com>
2022-07-20 13:40:09 -0300

    new file:   index.html 	modified:   main.js 	new file:   style.css

--- main.js
+++ main.js
@@ -6,9 +6,18 @@ const items = [
     quantidade: 0
     },
     {
-        id: 1,
-        nome: "camiseta2",
-        img: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        quantidade: 0
-        }
+    id: 1,
+    nome: "short",
+    img: "https://images.pexels.com/photos/1561011/pexels-photo-1561011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    quantidade: 0
+    }
 ]
+
+inicializarLoja = () => {
+    var containerProdutos = document.getElementById('produtos');
+    items.map((val)=>{
+        console.log(val.nome);
+    })
+}
+
+inicializarLoja();
