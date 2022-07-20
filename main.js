cdcc04886f9157535d90f47899652273eb0f090d
Joilson Izael <90202358+JoilsonI@users.noreply.github.com>
2022-07-20 13:22:21 -0300

    miniloja

---
+++ main.js
@@ -0,0 +1,14 @@
+const items = [
+    {
+    id: 0,
+    nome: "camiseta",
+    img: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "camiseta2",
+        img: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        quantidade: 0
+        }
+]
