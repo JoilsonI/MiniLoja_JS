9579d99d133f43b2e612763f36dfc6b03fa08b32
Joilson Izael <90202358+JoilsonI@users.noreply.github.com>
2022-07-21 15:59:02 -0300

    teste

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ const items = [
     {
     id: 0,
     nome: "camiseta",
-    img: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "roupas.jpg",
     quantidade: 0
     },
     {
     id: 1,
     nome: "short",
-    img: "https://images.pexels.com/photos/1561011/pexels-photo-1561011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "roupas.jpg",
     quantidade: 0
     },
     {
-        id: 2,
-        nome: "camiseta2",
-        img: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        quantidade: 0
-        },
-        {
-        id: 3,
-        nome: "short2",
-        img: "https://images.pexels.com/photos/1561011/pexels-photo-1561011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        quantidade: 0
-        }
+    id: 2,
+    nome: "camiseta2",
+    img: "roupas.jpg",
+    quantidade: 0
+    },
+    {
+    id: 3,
+    nome: "short2",
+    img: "roupas.jpg",
+    quantidade: 0
+    }
 ]
 
 inicializarLoja = () => {
@@ -32,13 +32,25 @@ inicializarLoja = () => {
         
         <div class="produto-single">
             <img src="`+val.img+`" />
-            <p>`+val.nome`</p>
-            <a key="`+val.id+`" href="">Adicionar ao carrinhi!</a>
+            <p>`+val.nome+`</p>
+            <a key="`+val.id+`" href="">Adicionar ao carrinho!</a>
         </div>
-        
-        
         `;
     })
 }
 
 inicializarLoja();
+
+atualizarCarrinho = () => {
+
+}
+
+var links = document.getElementsByName('a');
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click",function(){
+        let key = this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarCarrinho();
+    })
+}
