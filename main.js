1634b9a20ced01526c8a65ac4de42173b626cec7
Joilson Izael <90202358+JoilsonI@users.noreply.github.com>
2022-07-21 15:13:39 -0300

    img jpg

--- main.js
+++ main.js
@@ -10,13 +10,34 @@ const items = [
     nome: "short",
     img: "https://images.pexels.com/photos/1561011/pexels-photo-1561011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     quantidade: 0
-    }
+    },
+    {
+        id: 2,
+        nome: "camiseta2",
+        img: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        quantidade: 0
+        },
+        {
+        id: 3,
+        nome: "short2",
+        img: "https://images.pexels.com/photos/1561011/pexels-photo-1561011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        quantidade: 0
+        }
 ]
 
 inicializarLoja = () => {
     var containerProdutos = document.getElementById('produtos');
     items.map((val)=>{
-        console.log(val.nome);
+        containerProdutos.innerHTML+=`
+        
+        <div class="produto-single">
+            <img src="`+val.img+`" />
+            <p>`+val.nome`</p>
+            <a key="`+val.id+`" href="">Adicionar ao carrinhi!</a>
+        </div>
+        
+        
+        `;
     })
 }
 
