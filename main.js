bb59f611197d462b0a49ecaef2a188ef5e24fdb4
Joilson Izael <90202358+JoilsonI@users.noreply.github.com>
2022-07-21 18:21:51 -0300

    teste

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 const items = [
     {
     id: 0,
-    nome: "camiseta",
+    nome: "camiseta0",
     img: "roupas.jpg",
     quantidade: 0
     },
     {
     id: 1,
-    nome: "short",
+    nome: "camiseta1",
     img: "roupas.jpg",
     quantidade: 0
     },
     {
     id: 2,
     nome: "camiseta2",
-    img: "roupas.jpg",
-    quantidade: 0
-    },
-    {
-    id: 3,
-    nome: "short2",
     img: "roupas.jpg",
     quantidade: 0
     }
@@ -30,11 +24,11 @@ inicializarLoja = () => {
     items.map((val)=>{
         containerProdutos.innerHTML+=`
         
-        <div class="produto-single">
-            <img src="`+val.img+`" />
-            <p>`+val.nome+`</p>
-            <a key="`+val.id+`" href="">Adicionar ao carrinho!</a>
-        </div>
+            <div class="produto-single">
+                <img src="`+val.img+`" />
+                <p>`+val.nome+`</p>
+                <a key="`+val.id+`" href="">Adicionar ao carrinho!</a>
+            </div>
         `;
     })
 }
@@ -43,8 +37,8 @@ inicializarLoja();
 
 atualizarCarrinho = () => {
     var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
     items.map((val)=>{
-        
         if(val.quantidade > 0){
         containerCarrinho.innerHTML+=`
         
